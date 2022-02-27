8c497209b80730773468b75c6445288b2417070d
Arcioth <70841794+Arcioth@users.noreply.github.com>
2022-02-27 11:56:47 +0300

    İlk sürüm
    Anime botunun ilk sürümü

---
+++ GEREKENLER.js
@@ -0,0 +1,16 @@
+//ihtiyacınız olacak şeyler:
+//Python - 3.10.2
+
+//Pluginler ve fazlası:
+
+//discord
+//json
+//os
+//datetime
+//asyncio
+//PILLOW
+//BytesIO
+//numpy
+//pathlib
+
+//Yükledikten sonra kendi tokenini main.pyde en altta (+ yı silip yerine) yapıştır
